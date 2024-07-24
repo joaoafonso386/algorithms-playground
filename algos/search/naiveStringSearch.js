ac4fc57d11bbb0c101709d8f92b1d494aec6a2ed
João Afonso <joaoafonso386@gmail.com>
2024-07-24 03:16:26 +0100

    Add naive string search problem

--- algos/search/naiveStringSearch.js
+++ algos/search/naiveStringSearch.js
@@ -3,10 +3,25 @@
  * Function stringSearch takes 2 strings, a larger string a pattern to match against that string. 
  * Find in the larger string the amount of times the pattern repeats itself.
  * 
- * stringSearch("wowomgzom", "omg") // 2
+ * stringSearch("wowomgzomg", "omg") // 2
  * 
  */
 
 
 
-const stringSearch = (largeS, shortS) => {}
+const stringSearch = (large, short) => {
+    
+    let match = 0
+
+    for(let i = 0; i < large.length; i++) {
+        for(let j = 0; j < short.length; j++) {
+            if(large[i+j] !== short[j]) break
+            if(j === short.length - 1) match++          
+        }
+    }
+
+    return match
+
+}
+
+console.log(stringSearch("wowomgzomgconaomgkysomg", "omg"))
