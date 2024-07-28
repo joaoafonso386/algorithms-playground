cfa76380363881f8a2bad2e1b53b143c80747d37
João Afonso <joaoafonso386@gmail.com>
2024-07-29 00:23:08 +0100

    Update selection sort

--- algos/sorting/selectionSort.js
+++ algos/sorting/selectionSort.js
@@ -1,7 +1,9 @@
 /**
  *
  * Selection sort finds the minimum value of an iteration and then swaps the current value with the minimum.
- *
+ * Instead of storing the value we store the index of the val and then compare it to the iteration index (i !== min)
+ * If the values are not the same we swap
+ * 
  * Ex: [1,8,7,2,5] -> at the second iteration (val = 8) the minimum value will be 2, so swap 8 with 2, resulting in [1,2,7,8,5]
  *
  */
@@ -24,4 +26,4 @@ const selectionSort = (arr) => {
   return arr;
 };
 
-console.log(selectionSort([1, 4, 5, 8, 6, 2]));
+console.log(selectionSort([1, 4, 5, 24, 8, 6, 2]));
