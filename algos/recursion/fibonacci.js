32cc8e0ba6fa8b04e97dc1d78b72e877e1afe020
João Afonso <joaoafonso386@gmail.com>
2024-07-31 00:32:16 +0100

    Add n to fibonacci sequence problem

--- algos/recursion/fibonacci.js
+++ algos/recursion/fibonacci.js
@@ -6,11 +6,13 @@
  * 
  */
 
+let num = 10
+
 const fib = (n) => {
     if(n < 2) return n
     return fib(n - 1) + fib(n - 2)
 }
 
-for(let i = 0; i < 10; i++) {
+for(let i = 0; i < num; i++) {
     console.log(fib(i))
 }
