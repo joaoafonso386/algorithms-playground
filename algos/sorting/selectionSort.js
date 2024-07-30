8fe4a9aace6dfd5727e07dce656dba92f715f3fc
João Afonso <joaoafonso386@gmail.com>
2024-07-30 01:09:23 +0100

    Update selection sort explanation

--- algos/sorting/selectionSort.js
+++ algos/sorting/selectionSort.js
@@ -1,11 +1,14 @@
 /**
  *
  * Selection sort finds the minimum value of an iteration and then swaps the current value with the minimum.
- * Instead of storing the value we store the index of the val and then compare it to the iteration index (i !== min)
- * If the values are not the same we swap
+ * Instead of storing the value we store the index of the val. At each outer iteration we define a minimum of the first element (i) and then start comparing in each iteration to see if that minimum is surpassed.
  * 
- * Ex: [1,8,7,2,5] -> at the second iteration (val = 8) the minimum value will be 2, so swap 8 with 2, resulting in [1,2,7,8,5]
- *
+ * Ex: [1,8,7,2,5] -> to swap 8 with 2 the following happens: (look at the code for this. i starts at 0 and j at i +1 and min = i)
+ *     In the first iteration min = 0, then arr[0] > arr[1] makes min = 0
+ *     In the second iteration min = 1, then arr[1] > arr[2] makes min = 2
+ *     In the next iteration arr[2] > arr[3] makes min = 3
+ *     In the last iteration arr[3] > arr[4] min remains unchanged
+ *     We then swap arr[1] with arr[3] making 8 and 2 swap because i !== min
  */
 
 const selectionSort = (arr) => {
@@ -13,8 +16,8 @@ const selectionSort = (arr) => {
     let min = i
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[min] > arr[j]) {
-          min = j;
-        }
+        min = j;
+      }
     }
     if(i !== min) {
         let temp = arr[i]
