c9d6225851e0098cc013406bd0e02ea7476dd195
João Afonso <joaoafonso386@gmail.com>
2024-08-02 01:59:54 +0100

    Add insertion sort implementation

--- algos/sorting/insertionSort.js
+++ algos/sorting/insertionSort.js
@@ -1,5 +1,19 @@
-const insertionSort = (arr) => {
+/**
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
 
+const insertionSort = (arr) => {
+    for(let i = 1; i < arr.length; i++) {
+        const curr = arr[i]
+        for(var j = i - 1; j >= 0 && arr[j] > curr; j--) {
+            arr[j + 1] = arr[j]
+        }
+
+        arr[j + 1] = curr
+    }
+    return arr
 }
 
 
