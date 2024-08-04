0bf488ad5e247932f235ed664e779b77bea59d85
João Afonso <joaoafonso386@gmail.com>
2024-08-04 20:20:00 +0100

    Remove var usage from inner loop in insertion sort

--- algos/sorting/insertionSort.js
+++ algos/sorting/insertionSort.js
@@ -7,14 +7,13 @@
 const insertionSort = (arr) => {
     for(let i = 1; i < arr.length; i++) {
         const curr = arr[i]
-        for(var j = i - 1; j >= 0 && arr[j] > curr; j--) {
+        for(let j = i - 1; j >= 0 && arr[j] > curr; j--) {
             arr[j + 1] = arr[j]
+            arr[j] = curr
         }
-
-        arr[j + 1] = curr
     }
     return arr
 }
 
 
-console.log(insertionSort([4,19,5,2,54,10]))
+console.log(insertionSort([41,4,19,5,2,54,10]))
