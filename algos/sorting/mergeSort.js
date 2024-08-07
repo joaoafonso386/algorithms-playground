049e2dc88c48f9bf6593e055b2d3e53df53c7619
João Afonso <joaoafonso386@gmail.com>
2024-08-07 02:17:04 +0100

    Add docs and debugs to merge function

--- algos/sorting/mergeSort.js
+++ algos/sorting/mergeSort.js
@@ -1,12 +1,25 @@
+/**
+ * 
+ * @function merge
+ * @description merges 2 sorted arrays into 1 sorted array. Takes into account if the arrays are of different lengths.
+ *   
+ * @param {number[]} arr1 
+ * @param {number[]} arr2 
+ * @returns {number[]}
+ * 
+ */
+
 const  merge = (arr1, arr2) => {
     let results = [];
     let i = 0;
     let j = 0;
     while(i < arr1.length && j < arr2.length){
+        console.log(i < arr1.length || j < arr2.length, arr2.length, arr1.length, arr1, arr2)
         if(arr2[j] > arr1[i]){
             results.push(arr1[i]);
             i++;
         } else {
+            console.log("j", j, "val j", arr2[j], "i", i, "val i", arr1[i])
             results.push(arr2[j])
             j++;
         }
@@ -19,15 +32,14 @@ const  merge = (arr1, arr2) => {
         results.push(arr2[j])
         j++;
     }
-
     return results;
 }
 
-console.log(merge([2,1,4,5], [8,10,14,79,65,43,21,48]))
+console.log(merge([1,2,4,5], [8,10,14,21,43,48,65,79]))
 
 
 const mergeSort = (arr) => {
-
+    
     if(arr.length <= 1) return arr
     let mid = Math.floor(arr.length / 2)
     let left = mergeSort(arr.slice(0, mid))
@@ -36,6 +48,6 @@ const mergeSort = (arr) => {
 
 }
 
-// console.log(mergeSort([3,38,5,20,15,1,47]))
+console.log(mergeSort([3,38,5,20,15,1,47]))
 
 
