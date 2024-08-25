0ae19a74c431470cdd586799da5b9e3294cc8b0b
João Afonso <joaoafonso386@gmail.com>
2024-08-25 23:08:16 +0100

    update pivot function to quick sort

--- algos/sorting/quickSort.js
+++ algos/sorting/quickSort.js
@@ -4,24 +4,28 @@
 
 const pivot = (arr, start = 0, end = arr.length - 1) => {
 
-    const swap = (arr, i, j) => {
-        let temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-    }
-
     let pivot = arr[start]
     let swapIndex = start
 
     for(let i = start + 1; i <= end; i++) {
         if(pivot > arr[i]) {
             swapIndex++
-            swap(arr, swapIndex, i)
+            let temp = arr[swapIndex]
+            arr[swapIndex] = arr[i]
+            arr[i] = temp
         }
     }
+
+    let temp = arr[start]
+    arr[start] = arr[swapIndex]
+    arr[swapIndex] = temp
+    console.debug("Log array", arr)
+    return swapIndex
 
 }
 
 const quickSort = () => {
 
 }
+
+console.log(pivot([4,8,2,1,5,7,6,3]))
