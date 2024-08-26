e6b5bfb79aa352f5a0453e0ab48adec13b5a1c80
João Afonso <joaoafonso386@gmail.com>
2024-08-27 00:32:07 +0100

    Add helper function getDigit for radix sort

--- algos/sorting/radixSort.js
+++ algos/sorting/radixSort.js
@@ -1,3 +1,26 @@
+/**
+ * 
+ * Returns the digit in num at the given position provided. The solution bellow is guaranteed O(1) time complexity (most solutions are O(n)/O(log n))
+ * Ex: getDigit(12345, 0); // returns 5
+ * 
+ * 
+ * @param {number} num 
+ * @param {number} position 
+ * @returns {number} digit
+ */
+
+const getDigit = (num, position) => {
+    return Math.floor(Math.abs(num) / Math.pow(10, position) % 10)
+} 
+
+console.log(getDigit(8582,3))
+
+
+/**
+ * 
+ * @returns 
+ */
+
 const radixSort = () => {
     return
 }
