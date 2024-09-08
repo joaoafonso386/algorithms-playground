02285cfc76aeff45342512c4a2ff96a35b330ed4
João Afonso <joaoafonso386@gmail.com>
2024-09-08 23:11:52 +0100

    Add pop method to singly linked list

--- data-structures/singlyLinkedList.js
+++ data-structures/singlyLinkedList.js
@@ -16,6 +16,7 @@ class SinglyLinkedList {
         const node = new Node(val)
         if(!this.head) {
             this.head = node
+            //In the first item head and tail are the same
             this.tail = this.head
         } else {
             this.tail.next = node
@@ -24,10 +25,31 @@ class SinglyLinkedList {
         this.length++
         return this
     }
+
+    pop() {
+      if(!this.head) return undefined
+      let curr = this.head;
+      let newTail = curr
+      while(curr.next) {
+        newTail = curr
+        curr = curr.next
+      }
+      this.tail = newTail
+      this.tail.next = null
+      this.length--
+      if(this.length === 0) {
+        this.head = null
+        this.tail = null
+      }
+      return curr
+    }
+
+
 }
 
 const list = new SinglyLinkedList()
 list.push(5)
 list.push(10)
 list.push(23)
+console.log(list.pop())
 console.log(list)
