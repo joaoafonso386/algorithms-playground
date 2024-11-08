f1505b21f789814348a1ff0063324ed7dbccb5c3
João Afonso <joaoafonso386@gmail.com>
2024-11-08 02:23:39 +0000

    structure queue class

---
+++ data-structures/queues.js
@@ -0,0 +1,29 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = val
+    }
+
+}
+
+class Queue {
+
+    constructor() {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+
+    //add to the end
+    enqueue(){
+
+    }
+
+    //remove from the beginning
+    dequeue() {
+
+    }
+}
+
+const q = new Queue()
+console.log(q)
