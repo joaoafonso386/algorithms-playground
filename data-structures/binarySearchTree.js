bd5fa83cef64a26b7c3ee65807c3778c2b2714e9
João Afonso <joaoafonso386@gmail.com>
2024-11-10 18:28:54 +0000

    init binary search tress

---
+++ data-structures/binarySearchTree.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * In binary search trees each node has up to two child nodes, referred to as the left and right child.
+ * All nodes in the left subtree of a node contain values that are less than the node's value
+ * All nodes in the right subtree of a node contain values that are greater than the node's value.
+ * 
+ * Time complexity: O(log n)
+ * 
+ */
