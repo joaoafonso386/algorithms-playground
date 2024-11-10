cde78f29ef0886d385427fa64310e434aba61acb
João Afonso <joaoafonso386@gmail.com>
2024-11-10 19:12:39 +0000

    add insert method to binary search tree data strucutre

--- data-structures/binarySearchTree.js
+++ data-structures/binarySearchTree.js
@@ -20,9 +20,38 @@ class BinarySearchTree {
     constructor(){
         this.root = null    
     }
+
+    insert(val) {
+        const node = new Node(val)
+        if(!this.root) { 
+            this.root = node
+            return this 
+        }
+        let root = this.root
+        while(true) {
+            if(node.value > root.value) {
+                if(!root.right) {
+                    root.right = node
+                    return this
+                }
+                root = root.right
+            } else {
+                if(!root.left) {
+                    root.left = node
+                    return this
+                }
+                root = root.left
+            }
+        }
+
+    }
 }
 
 const tree = new BinarySearchTree()
 tree.root = new Node(10)
 tree.root.right = new Node(75)
 tree.root.left = new Node(7)
+tree.root.left.right = new Node(8)
+tree.root.left.left = new Node(5)
+tree.insert(6)
+console.log(tree.root.left)
