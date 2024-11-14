f5d467528074b050307f6b8ea6ffc8355cb127bc
João Afonso <joaoafonso386@gmail.com>
2024-11-14 01:53:03 +0000

    init binary heaps

---
+++ data-structures/binaryHeaps.js
@@ -0,0 +1,13 @@
+/**
+ *
+ *  A binary heap where each node has at most two children, and it's typically used in implementing priority queues.
+ *  Max Binary Heap: A binary heap where each parent node has a value greater than or equal to its children.
+ *  Min Binary Heap: A binary heap where each parent node has a value less than or equal to its children.
+ *
+ *  Time Complexity
+ *  Insertion and removal: O(log n), where n is the number of nodes.
+ *  Accessing the root (min or max): O(1).
+ *
+ */
+
+
