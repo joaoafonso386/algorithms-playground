34ee3d1c4895c87afd5c588521dda4efbd5acc0a
João Afonso <joaoafonso386@gmail.com>
2024-11-20 00:54:53 +0000

    update hash table data structure and join hash function in the same class

--- data-structures/hashTable.js
+++ data-structures/hashTable.js
@@ -4,20 +4,26 @@
  * - A data structure that maps keys to values for efficient lookups.
  * - Uses a hash function to compute an index for storing keys in an array.
  * - Offers average O(1) time complexity for insert, delete, and lookup operations.
- * - Handles collisions using methods like chaining or open addressing.
+ * - Handles collisions using methods like separate chaining (store multiple values in each position) or linear probing (1 value for each position).
  * - Commonly used for implementing caches, dictionaries, and sets.
  * 
  */
 
-const hash = (key, length) => {
-    let total = 0
-    let WEIRD_PRIME = 31;
-    for(let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i]
-        let val = char.charCodeAt(0) - 96 
-        total = (total * WEIRD_PRIME + val) % length
-    } 
 
-    return total
+class HashTable {
+    constructor(size = 53) {
+        this.keyMap = new Array(size)
+    }
 
+    _hash(key) {
+        let total = 0
+        let WEIRD_PRIME = 31;
+        for(let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let val = char.charCodeAt(0) - 96 
+            total = (total * WEIRD_PRIME + val) % this.keyMap.length
+        } 
+    
+        return total
+    }
 }
